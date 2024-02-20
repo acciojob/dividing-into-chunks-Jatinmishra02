1d2616911adf7ea1342b48b8fccdde46da57f066
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-02-20 20:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let currentChunk = [];
+  let currentSum = 0;
+
+  arr.forEach(num => {
+    if (currentSum + num <= n) {
+      currentChunk.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentChunk);
+      currentChunk = [num];
+      currentSum = num;
+    }
+  });
+
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
 };
 
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+alert(JSON.stringify(divide(arr, parseInt(n))));
